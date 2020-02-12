ccb49a5cea1afa2d782ee6fb4e2db80a2244a1c1
Sushil <sushilmangal731@gmail.com>
2020-02-12 11:31:29 +0000

    using useEffect to replace componentDidMount

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -19,22 +19,10 @@ const Search = ({ setAlert, searchUsers, showClear, clearUser }) => {
   return (
     <div>
       <form onSubmit={onSubmit} className='form'>
-        <input
-          type='text'
-          name='text'
-          placeholder='Search Users...'
-          value={text}
-          onChange={onChange}
-        />
-        <input
-          type='submit'
-          value='Search'
-          className='btn btn-dark btn-block'
-        />
+        <input type='text' name='text' placeholder='Search Users...' value={text} onChange={onChange} />
+        <input type='submit' value='Search' className='btn btn-dark btn-block' />
         {showClear && (
-          <button className='btn btn-light btn-block' onClick={clearUser}>
-            Clear
-          </button>
+          <button className='btn btn-light btn-block' onClick={clearUser}>Clear</button>
         )}
       </form>
     </div>
